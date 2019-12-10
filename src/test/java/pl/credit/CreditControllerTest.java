6a3bebcaba178a3efa1c3938d3ec8d9186f88030
GREG <rr.grzesiek@gmail.com>
2019-12-10 01:04:42 +0100

    update tests

--- src/test/java/pl/credit/CreditControllerTest.java
+++ src/test/java/pl/credit/CreditControllerTest.java
@@ -1,22 +1,22 @@
-package pl.credit;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import pl.credit.controller.CreditController;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(CreditController.class)
-public class CreditControllerTest {
+//package pl.credit;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mockito;
+//import org.skyscreamer.jsonassert.JSONAssert;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.RequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import pl.credit.controller.CreditController;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(CreditController.class)
+//public class CreditControllerTest {
 
 //	@Autowired
 //	private MockMvc mockMvc;
@@ -43,4 +43,4 @@ public class CreditControllerTest {
 //		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
 //	}
 
-}
+//}
